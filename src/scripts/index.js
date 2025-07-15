5cc33d1c3df727456644e9cb39ff6fe73a631dc3
unknown <priyanshusolon2001@gmail.com>
2025-07-15 21:44:57 +0530

    ok

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,8 +18,8 @@ function LoadDashboard(){
                                <p> ${appointment.description} </p>
                                <div class="bi bi-calendar"> ${appointment.date.slice(0, appointment.date.indexOf("T"))}</div>
                                <div class="mt-3">
-                                  <button class="bi bi-pen-fill btn btn-warning m-x2"></button>
-                                  <button class="bi bi-trash btn btn-danger m-x2"></button>
+                                  <button value=${appointment.appointment_id} id="btnEdit" class="bi bi-pen-fill btn btn-warning m-x2"></button>
+                                  <button value=${appointment.appointment_id} id="btnDelete" class="bi bi-trash btn btn-danger m-x2"></button>
                                </div>
                             </div>`).appendTo("#appointments");
                      })
@@ -127,6 +127,87 @@ $(function(){
 
     $(document).on("click", "#btnCancel",()=>{
          LoadPage('user_dashboard.html');
+    })
+
+    // Add Appointment
+
+    $(document).on("click", "#btnAdd", ()=>{
+
+          var appointment = {
+               appointment_id : $("#appointment_id").val(),
+               title: $("#title").val(),
+               description: $("#description").val(),
+               date: $("#date").val(),
+               user_id: $.cookie("userid")
+          }
+          $.ajax({
+            method:"post",
+            url: `http://127.0.0.1:4040/add-appointment`,
+            data: appointment
+          })
+          alert('Appointment Added');
+          LoadDashboard();
+
+    })
+
+    // Edit Click
+    $(document).on("click", "#btnEdit",(e)=>{
+
+          LoadPage("edit_appointment.html");
+        
+          $.ajax({
+            method: "get", 
+            url: `http://127.0.0.1:4040/appointment/${e.target.value}`,
+            success: (appointment=>{
+                   $("#appointment_id").val(appointment.appointment_id),
+                   $("#title").val(appointment.title),
+                   $("#description").val(appointment.description),
+                   $("#date").val(appointment.date.slice(0, appointment.date.indexOf("T")))
+                   sessionStorage.setItem("appointment_id", appointment.appointment_id);
+            })
+          })
+         
+    })
+
+    $(document).on("click", "#btnEditCancel",()=>{
+          LoadDashboard();
+    })
+
+
+    // Save Click
+
+    $(document).on("click", "#btnSave", (e)=>{
+
+          var appointment = {
+               appointment_id : $("#appointment_id").val(),
+               title: $("#title").val(),
+               description: $("#description").val(),
+               date: $("#date").val(),
+               user_id: $.cookie("userid")
+          }
+          $.ajax({
+            method:"put",
+            url: `http://127.0.0.1:4040/edit-appointment/${sessionStorage.getItem("appointment_id")}`,
+            data: appointment
+          })
+          alert('Appointment Updated Successfully.');
+          LoadDashboard();
+
+    })
+
+    // Delete Click
+
+     $(document).on("click", "#btnDelete",(e)=>{
+
+          var choice = confirm('Are you sure? Want to Delete?');
+          if(choice===true){
+              $.ajax({
+                    method: "delete", 
+                    url: `http://127.0.0.1:4040/delete-appointment/${e.target.value}`,
+                })
+                alert('Appointment Deleted..');
+                LoadDashboard();
+          }
     })
 
 })
